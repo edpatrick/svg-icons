bac54c2ce75194495c79c9d5b402b07828ceb959
Ed Patrick <patricke@CILIP-LONDON.local>
2016-10-28 11:42:44 +0100

    animate cogs

---
+++ js/min/animate.js
@@ -0,0 +1,1 @@
+function rotateCogs(){TweenMax.to("#cog-large",2,{rotation:"+=270",transformOrigin:"center"}),TweenMax.to("#cog-medium",2,{rotation:"-=270",transformOrigin:"center"}),TweenMax.to("#cog-small",2,{rotation:"-=270",transformOrigin:"center"})}
